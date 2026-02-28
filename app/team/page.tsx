5b7e4aceca17dc6809e4e999325d27181286ec58
Tanmoy mandal <tanmaymandal1250@gmail.com>
2026-02-28 15:21:46 +0530

    background added in the team page

---
+++ app/team/page.tsx
@@ -0,0 +1,13 @@
+import LightFXBackground from '@/components/LightFXBackground';
+
+export default function TeamPage() {
+  return (
+    <>
+      <LightFXBackground />
+      <div className="relative z-10">
+        {/* Your team page content goes here */}
+        <h1 className="text-4xl font-bold">Team</h1>
+      </div>
+    </>
+  );
+}
