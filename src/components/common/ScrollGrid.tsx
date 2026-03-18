a37c569a627c6d54abe428d70941b59a0aa242bb
Vidhyanshu Kumar <vidhyanshukumarvidhyam@gmail.com>
2026-03-18 16:23:42 +0530

    scroll grid

--- src/components/common/ScrollGrid.tsx
+++ src/components/common/ScrollGrid.tsx
@@ -260,7 +260,7 @@ export default function ScrollGrid() {
                     </div>
                 </motion.div>
             </div>
-            <br /><br /><br /><br /><br />
+            <br /><br />
         </div>
         
     );
